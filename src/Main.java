103760ba3bfa18536e1bdaac2bede4211a9f9706
korrova11 <korrova@yandex.ru>
2023-08-24 17:07:54 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,32 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         String str[] = {"Как", "здорово", "Как", "здорово", "учиться", "!"};
+        int[] chin = {5, 6, 5, 8, 6};
         defDouble(str);
+
     }
-    public static void defDouble(String []strn) {
-        for (int i= 0; i < strn.length -1 ;i ++) {
+
+    public static void defDouble(String[] strn) {
+        for (int i = 0; i < strn.length - 1; i++) {
             if (strn[i].equals(""))
                 continue;
             else
-                for (int j = 0; j < strn.length ; j++) {
+                for (int j = i + 1; j < strn.length; j++) {
                     if (strn[i].equals(strn[j]))
                         strn[j] = "";
 
                 }
+
         }
-        System.out.println(strn[0] +strn[1]);
-        System.out.println("привет");
+        String st = "";
+        for (String str:strn) {
+            st += str;
+        }
+        System.out.println(st);
 
-    }
-    }
-
-
-
+            }
+                   }
 
