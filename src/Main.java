296f07ce2072242bee3b313c61998bf3eba97639
korrova11 <korrova@yandex.ru>
2023-08-24 14:40:24 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String str[] = {"Как", "здорово", "Как", "здорово", "учиться", "!"};
+        defDouble(str);
     }
-}
+    public static void defDouble(String []strn) {
+        for (int i= 0; i < strn.length -1 ;i ++) {
+            if (strn[i].equals(""))
+                continue;
+            else
+                for (int j = 0; j < strn.length ; j++) {
+                    if (strn[i].equals(strn[j]))
+                        strn[j] = "";
+
+                }
+        }
+        System.out.println(strn[0] +strn[1]);
+        System.out.println("привет");
+
+    }
+    }
+
+
+
+
